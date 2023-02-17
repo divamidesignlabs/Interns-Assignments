b21ff42cb85579b81d31c2a1f371f36fd18a5494
sid <sidharth@divami.com>
2023-02-17 23:36:03 +0530

    feat: service configured and interface for movie data ready

---
+++ movies/src/app/models/movie-data.ts
@@ -0,0 +1,29 @@
+export interface MovieData {
+  _id: number;
+
+  name: string;
+
+  synopsis: string;
+
+  release_date: Date;
+
+  released: string;
+
+  poster_url: string;
+
+  trailer_url: string;
+
+  pg_rating: string;
+
+  language: string;
+
+  genre: string;
+
+  duration: number;
+
+  like_count: number;
+
+  dislike_count: number;
+
+  show_times: string;
+}
