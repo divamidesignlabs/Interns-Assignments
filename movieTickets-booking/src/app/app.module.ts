5a7269ef8f08b0d4f2f91848ddf6dfbc12b2c177
saii phawhan <saii@divami.com>
2023-02-18 04:44:48 +0530

    movie details page

--- movieTickets-booking/src/app/app.module.ts
+++ movieTickets-booking/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BookingPageComponent } from './booking-page/booking-page.component';
+import { MovieTrailerComponent } from './common-components/movie-trailer/movie-trailer.component';
+import {VgCoreModule} from '@videogular/ngx-videogular/core';
+import {VgControlsModule} from '@videogular/ngx-videogular/controls';
+import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
+import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingPageComponent,
+    MovieTrailerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
