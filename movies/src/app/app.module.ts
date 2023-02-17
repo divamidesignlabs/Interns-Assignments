2c250f3ef73eb7dcb46577fbacd78e9d43547571
sai <sai@example.com>
2023-02-18 02:39:00 +0530

    feat: developed card component.

--- movies/src/app/app.module.ts
+++ movies/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieCardComponent } from './shared/movie-card/movie-card.component';
-
+import {TimeConverter} from 'src/app/shared/movie-card/movie-card.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MovieCardComponent
+    MovieCardComponent,
+    TimeConverter
   ],
   imports: [
     BrowserModule,
