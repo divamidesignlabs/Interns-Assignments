cdb7f642382f36e8705a5180c2b936beafcfc6d0
vaishnavi <vaishnavi@divami.com>
2023-02-18 06:02:58 +0530

    header developed

--- movieTickets-booking/src/app/app.module.ts
+++ movieTickets-booking/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common-components/header/header.component';
-
+import { LogoComponent } from './common-components/logo/logo.component';
+import { SearchbarComponent } from './common-components/searchbar/searchbar.component';
+import { DashboardComponent } from './common-components/dashboard/dashboard.component';
+import { FormControl, FormGroup} from '@angular/forms'
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoComponent,
+    SearchbarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
