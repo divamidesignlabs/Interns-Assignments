581e51a958efb8abe523bc815b8ed5ddd836b792
sai <sai@example.com>
2023-02-18 06:49:11 +0530

    updated tabs component

--- movies/src/app/app.module.ts
+++ movies/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
@@ -6,18 +6,35 @@ import { AppComponent } from './app.component';
 import { CarouselComponent } from './shared/carousel/carousel.component';
 import { MovieCardComponent } from './shared/movie-card/movie-card.component';
 import {TimeConverter} from 'src/app/shared/movie-card/movie-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { CommonModule } from '@angular/common';
+import {MatTabsModule} from '@angular/material/tabs';
+import { TabsComponent } from './shared/tabs/tabs.component';
+// import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
 @NgModule({
   declarations: [
     AppComponent,
     MovieCardComponent,
     TimeConverter,
-    CarouselComponent
+    CarouselComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule   
+    HttpClientModule,
+    BrowserAnimationsModule  ,
+    MaterialModule ,
+    MatInputModule,
+    MatAutocompleteModule,
+    CommonModule ,
+    MatTabsModule
+    
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ movies/src/app/shared/tabs/tabs.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss']
+})
+export class TabsComponent {
+//   isEnableNow=true;
+//   isEnableUpcoming=false;
+// enableNowShowing(){
+// this.isEnableNow=true;
+// this.isEnableUpcoming=false;
+// }
+// enableUpcoming(){
+// this.isEnableNow=false;
+// this.isEnableUpcoming=true;
+// }
+}
